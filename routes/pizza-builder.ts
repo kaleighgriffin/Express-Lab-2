485486587b8424259f19d31398116d0b6e0fdd7e
kaleighgriffin <kaleigh200@gmail.com>
2021-05-11 09:21:10 -0400

    added price

--- routes/pizza-builder.ts
+++ routes/pizza-builder.ts
@@ -13,7 +13,16 @@ routes.get("/pizza-builder-results", (req, res) => {
     const toppings: number = parseInt(req.query.toppings as string);
     const glutenFree: boolean = !!req.query.glutenFree;
     const specialInstructions = req.query.specialInstructions;
-    res.render("pizza-builder-results", { size, toppings, glutenFree, specialInstructions });
+    let price = 0;
+    if (size === "Small") {
+        price = 7 + toppings * .5;
+    } else if (size === "Medium") {
+        price = 10 + toppings * 1;
+    } else {
+        price = 12 + toppings * 1.25;
+    }
+    let displayPrice = price.toFixed(2);
+    res.render("pizza-builder-results", { size, toppings, glutenFree, specialInstructions, displayPrice });
 });
 
 export default routes;
